94f586eea3c714418807d0de21a62fadf46b542c
silverweed <silverweed1991@gmail.com>
2017-03-01 15:40:03 +0100

    update newpost.sh

--- newpost.sh
+++ newpost.sh
@@ -3,7 +3,9 @@ POSTNAME=$(dirname $(readlink -f $0))/_posts/$(date +%Y-%m-%d-$(tr ' ' '_' <<<$*
 cat >$POSTNAME <<EOF
 ---
 title: $*
+author: silverweed
 layout: single
+tags:
 ---
 EOF
 [[ $? == 0 ]] && vim $POSTNAME
